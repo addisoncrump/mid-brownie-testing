3c3e0d48dfb26930fe038ef079160b63706da0c3
Caleb Jasik <calebjasik@jasik.xyz>
2025-01-06 23:45:51 -0600

    mmmmm vite
    mmmmm vite

--- web/www/bootstrap.js
+++ web/www/bootstrap.js
@@ -1,24 +1,24 @@
 init();
 
 async function init() {
-  if (typeof process == "object") {
-    // We run in the npm/webpack environment.
-    const [{ Chart }, { main, setup }] = await Promise.all([
-      import("wasm-demo"),
-      import("./index.js"),
-    ]);
-    setup(Chart);
-    main();
-  } else {
-    const [{ Chart, default: init, initThreadPool }, { main, setup }] =
-      await Promise.all([import("../pkg/web.js"), import("./index.js")]);
-    // Regular wasm-bindgen initialization.
-    await init();
+  //   if (typeof process == "object") {
+  //     // We run in the npm/webpack environment.
+  //     const [{ Chart }, { main, setup }] = await Promise.all([
+  //       import("wasm-demo"),
+  //       import("./index.js"),
+  //     ]);
+  //     setup(Chart);
+  //     main();
+  //   } else {
+  const [{ Chart, default: init, initThreadPool }, { main, setup }] =
+    await Promise.all([import("../pkg/web.js"), import("./index.js")]);
+  // Regular wasm-bindgen initialization.
+  await init();
 
-    // Thread pool initialization with the given number of threads
-    // (pass `navigator.hardwareConcurrency` if you want to use all cores).
-    await initThreadPool(navigator.hardwareConcurrency);
-    setup(Chart);
-    main();
-  }
+  // Thread pool initialization with the given number of threads
+  // (pass `navigator.hardwareConcurrency` if you want to use all cores).
+  await initThreadPool(navigator.hardwareConcurrency);
+  setup(Chart);
+  main();
+  //   }
 }
